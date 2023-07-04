8ca88172ff1b88b793b1c31f3106c5e8d6dad83b
Jason Ernst <ernstjason1@gmail.com>
2023-07-04 16:08:30 -0700

    Show the chart and stats

--- src/ping4/index.php
+++ src/ping4/index.php
@@ -11,16 +11,22 @@ showHeader();
           <input type="button" value="Ping Ipv4 host" onclick="document.getElementById('ping4').submit();" class="btn btn-primary">
         </form>
       </div>
-      <?php if (isset($_GET["host"]) && $_GET["host"] != "") { ?>
+<?php if (isset($_GET["host"]) && $_GET["host"] != "") { ?>
       <div class="row pt-5">
-        <pre><?php $output = ping4($_GET["host"]); echo $output; ?></pre>
+        <pre><?php $result = ping4($_GET["host"]); echo $result->rawResult; ?></pre>
       </div>
-    <?php } else { ?>
+      <div class="row pt-5" id="chart">
+        <?php last24HoursPingResults($result); ?>
+      </div>
+      <div class="row pt-5">
+        <?php statComparison($result); ?>
+      </div>
+<?php } else { ?>
       <div class="row pt-5">
         <p>Performs an ICMPv4 ping request using this online tool. For example, try <a href="?host=8.8.8.8">8.8.8.8</a> or <a href="?host=google.com"/>google.com</a> to check their ping4 response.</p>
         <p>Interested in a ping6 request instead? Try <a href="https://ping6.network">ping6.network</a> instead.</p>
       </div>
-    <?php } ?>
+<?php } ?>
     </div>
   </body>
 </html>
