9fa8e58fd6b4c8479a6e98882a953614671af820
Jason Ernst <ernstjason1@gmail.com>
2023-07-06 17:40:59 -0700

    Added the stats page

---
+++ src/ping4/stats.php
@@ -0,0 +1,18 @@
+<?php
+require_once("functions.php");
+showHeader("Stats");
+?>
+<body class="d-flex">
+  <div class="mx-auto flex-column w-50">
+    <div class="row">
+      <h1 class="text-center pt-5 logo"><a href="<?php echo reconstruct_url(); ?>">ping4.network</a></h1>
+      <h2 class="text-center logo">Stats</h2>
+    </div>
+    <div class="row pt-5">
+      <?php globalStats(); ?>
+    </div>
+    <div class="row pt-5">
+      <?php lastPings(10); ?>
+    </div>
+  </div>
+</body>
