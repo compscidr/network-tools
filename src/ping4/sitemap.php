36baef5c9b7be398e7514b271259a1fbe344defd
Jason Ernst <ernstjason1@gmail.com>
2023-07-12 08:00:11 -0700

    fix date format for sitemap

--- src/ping4/sitemap.php
+++ src/ping4/sitemap.php
@@ -5,13 +5,13 @@ header('Content-Type: application/xml');
 <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1" xmlns:geo="http://www.google.com/geo/schemas/sitemap/1.0" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:pagemap="http://www.google.com/schemas/sitemap-pagemap/1.0" xmlns:xhtml="http://www.w3.org/1999/xhtml" >
   <url>
     <loc>https://www.ping4.network/</loc>
-    <lastmod><?php echo date('Y-m-dTH:i:sP', time()); ?></lastmod>
+    <lastmod><?php echo date('Y-m-d\TH:i:sP', time()); ?></lastmod>
     <changefreq>weekly</changefreq>
     <priority>1</priority>
   </url>
   <url>
     <loc>https://www.ping4.network/stats.php</loc>
-    <lastmod><?php echo date('Y-m-dTH:i:sP', time()); ?></lastmod>
+    <lastmod><?php echo date('Y-m-d\TH:i:sP', time()); ?></lastmod>
     <changefreq>daily</changefreq>
     <priority>0.9</priority>
   </url>
